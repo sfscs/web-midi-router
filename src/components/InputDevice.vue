190e5a67166a353f5661b09a250a980ceeae76f6
sfscs <jacobneilgustafson@gmail.com>
2019-03-02 20:18:31 -0800

    initial commit

---
+++ src/components/InputDevice.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Input port [type:'"
+    {{input.type}}
+    "'] id:'"
+    {{ input.id }}
+    "' manufacturer:'"
+    {{ input.manufacturer }}
+    "' name:'"
+    {{ input.name }}
+    "' version:'"
+    {{ input.version }}
+    "'"
+  </div>
+</template>
+<script>
+export default {
+  props: ["input", "idx"]
+};
+</script>
